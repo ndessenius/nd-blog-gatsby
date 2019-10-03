6b67550eda4d47b4b809dc3bdd5dca92342c6411
unknown <nd.managment@gmail.com>
2019-10-03 19:32:56 +0200

    Cleaaaaan

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import floppy from '../images/floppy.png'
 import avatar from '../../content/images/avatar.png'
 import sun from '../images/sun.svg'
 import moon from '../images/moon.svg'
-import kofi from '../../content/images/kofi3.png'
+import kofi from '../../content/thumbnails/kofi.png'
 import ThemeContext from '../context/ThemeContext'
 
 export default class Navigation extends Component {
